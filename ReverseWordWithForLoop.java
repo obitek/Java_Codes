48442b2b9481ad15acdc348950f1170a071bee27
Obinna Unah <unahobinna@gmail.com>
2025-06-28 22:05:07 +0100

    Changed the while loop to for loop

--- ReverseWordWithForLoop.java
+++ ReverseWordWithForLoop.java
@@ -1,14 +1,15 @@
 
+
 import java.util.Scanner;                    // import the scanner class
 
 public class ReverseWordWithForLoop {
    public static void main(String[] args){
       Scanner obj = new Scanner(System.in);  // create scanner object
-      System.out.println("This program reverses every word inputted");
-      System.out.print("Enter a word (or yes/y to quit): ");
-      String OriginalWord = obj.nextLine();          // store original word
+      System.out.println("This program reverses every word keyed in.");
       
-      while(!(OriginalWord.equalsIgnoreCase("yes") || OriginalWord.equalsIgnoreCase("y"))){
+      for(;;){                                          // loop forever
+         System.out.print("Enter a word: ");
+         String OriginalWord = obj.nextLine();          // store original word
          String rev = "";
          for(int i = OriginalWord.length() - 1; i > -1; i--){
             rev += OriginalWord.charAt(i);
@@ -16,8 +17,11 @@ public class ReverseWordWithForLoop {
          System.out.println("The original word is: \t" + OriginalWord);
          System.out.println("The reversed word is: \t" + rev);
          System.out.println();
-         System.out.print("Enter a word (or yes/y to quit): ");
-         OriginalWord = obj.nextLine();      // store original word
+         System.out.print("Do you want to quit? (yes/y): ");
+         String check = obj.nextLine();
+         if(check.equalsIgnoreCase("yes") || check.equalsIgnoreCase("y")){
+            break;
+         }
       }
       obj.close();
    }
